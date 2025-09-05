a8005dc9c27105b9d6be708444af5488e39661d7
Schmelhaus Norbert <sch.norbeee@gmail.com>
2025-09-05 20:18:02 +0200

    Clean test code

---
+++ src/test/java/com/dynata/surveyhw/controllers/AbstractIntegrationTest.java
@@ -0,0 +1,62 @@
+package com.dynata.surveyhw.controllers;
+
+import com.dynata.surveyhw.utils.InitFullState;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ActiveProfiles("test")
+@Testcontainers
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@DirtiesContext
+public abstract class AbstractIntegrationTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private InitFullState initFullState;
+
+    @Container
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("user")
+            .withPassword("password");
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", () ->
+                String.format("r2dbc:postgresql://%s:%d/%s",
+                        postgres.getHost(),
+                        postgres.getMappedPort(5432),
+                        postgres.getDatabaseName())
+        );
+        registry.add("spring.r2dbc.username", postgres::getUsername);
+        registry.add("spring.r2dbc.password", postgres::getPassword);
+    }
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = port;
+    }
+
+    @AfterEach
+    void cleanup() {
+        initFullState.deleteFullDatabase();
+    }
+}
--- src/test/java/com/dynata/surveyhw/controllers/MemberControllerTest.java
+++ src/test/java/com/dynata/surveyhw/controllers/MemberControllerTest.java
@@ -4,74 +4,22 @@ import com.dynata.surveyhw.repositories.MemberRepository;
 import com.dynata.surveyhw.utils.InitFullState;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 
-@ActiveProfiles("test")
-@Testcontainers
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = DEFINED_PORT, properties = {
-        "server.port=8082"
-})
-@DirtiesContext
-public class MemberControllerTest {
-
-    @LocalServerPort
-    int port;
+public class MemberControllerTest extends AbstractIntegrationTest {
 
     @Autowired
     private MemberRepository memberRepository;
 
     @Autowired
     private InitFullState initFullState;
-
-    @Container
-    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
-            .withDatabaseName("testdb")
-            .withUsername("user")
-            .withPassword("password");
-
-    @DynamicPropertySource
-    static void overrideProps(DynamicPropertyRegistry registry) {
-        registry.add("spring.r2dbc.url", () ->
-                String.format("r2dbc:postgresql://%s:%d/%s",
-                        postgres.getHost(),
-                        postgres.getMappedPort(5432),
-                        postgres.getDatabaseName())
-        );
-        registry.add("spring.r2dbc.username", postgres::getUsername);
-        registry.add("spring.r2dbc.password", postgres::getPassword);
-    }
-
-    @BeforeEach
-    void setup() {
-        RestAssured.port = port;
-    }
-
-    @AfterEach
-    void cleanup() {
-        initFullState.deleteFullDatabase();
-    }
 
     @Test
     @Order(1)
